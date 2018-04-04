e8a597e56578db081cee5cd5519eb0d16dd7af16
oleg_cherednichenko <oleg.a.cherednichenko@gmail.com>
2018-04-04 13:27:29 +0300

    commit

--- src/main/java/com/javase/param_class/SimpleActionCourse.java
+++ src/main/java/com/javase/param_class/SimpleActionCourse.java
@@ -1,9 +1,13 @@
 package com.javase.param_class;
 
-public class SimpleActionCourse extends Course {
+public class SimpleActionCourse <T1> extends Course {
 
-    public <T1 extends Course> SimpleActionCourse(T1 course) {
+    public <T1 extends Course> SimpleActionCourse(T1 course) {}
 
+    public <T2> SimpleActionCourse() {}
+
+    public <T3 extends Course> float calcMark () {
+        return 0.1f;
     }
 
 }
