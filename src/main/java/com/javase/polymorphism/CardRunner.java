73e63741005e28087d8c85dbd299819a2d5b4732
oleg_cherednichenko <oleg.a.cherednichenko@gmail.com>
2018-04-06 19:54:49 +0300

    commit

--- src/main/java/com/javase/polymorphism/CardRunner.java
+++ src/main/java/com/javase/polymorphism/CardRunner.java
@@ -1,4 +1,13 @@
 package com.javase.polymorphism;
 
 public class CardRunner {
+    public static void run() {
+        CardAction dc1 = new CardAction();
+        CardAction dc2 = new CreditCardAction();
+        CreditCardAction cc = new CreditCardAction();
+        dc1.doPayment(15.5);
+        dc2.doPayment(21.2);
+        cc.doPayment(7.0);
+        cc.checkCreditLimit();
+    }
 }
--- src/test/java/MockitoTestRunner.java
+++ src/test/java/MockitoTestRunner.java
@@ -1,6 +1,15 @@
+import com.javase.simple_mvc.model.Account;
+import com.javase.simple_mvc.model.Person;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MockitoTestRunner {
+    @Mock
+    private Account account;
+
+    @InjectMocks
+    private Person person = new Person();
 }
