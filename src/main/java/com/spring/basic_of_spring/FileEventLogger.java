24fbfc836a72372117d7cc2abf2d4f4a197f1643
oleg_cherednichenko <oleg.a.cherednichenko@gmail.com>
2018-04-24 20:08:18 +0300

    commit

--- src/main/java/com/spring/basic_of_spring/FileEventLogger.java
+++ src/main/java/com/spring/basic_of_spring/FileEventLogger.java
@@ -1,6 +1,11 @@
 package com.spring.basic_of_spring;
 
 public class FileEventLogger implements EventLogger {
+
+    public FileEventLogger(String filename) {
+        this.filename = filename;
+    }
+
     private String filename;
 
     @Override
