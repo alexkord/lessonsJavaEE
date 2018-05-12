5b4a81e1c2b4b2b0bb1bbb8c747d3fd471d70a21
oleg_cherednichenko <oleg.a.cherednichenko@gmail.com>
2018-05-12 18:19:10 +0300

    commit

--- src/main/java/com/spring/ch3/DeclareSpringComponents.java
+++ src/main/java/com/spring/ch3/DeclareSpringComponents.java
@@ -1,5 +1,6 @@
 package com.spring.ch3;
 
+import com.spring.ch3.xml.ConstructorConfusion;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class DeclareSpringComponents {
@@ -8,12 +9,16 @@ public class DeclareSpringComponents {
 
         ctx.load("classpath:app-context-annotation.xml");
         ctx.refresh();
-        MessageProvider provider = (MessageProvider) ctx.getBean("message", MessageProvider.class);
-        System.out.println(provider.getMessage());
-//        GenericXmlApplicationContext context = new GenericXmlApplicationContext();
-//        context.load("classpath:app-context-annotation.xml");
-//        context.refresh();
-//        MessageProvider provider = (MessageProvider) context.getBean("messageProvider", MessageProvider.class);
+//        MessageProvider provider = (MessageProvider) ctx.getBean("messageProviderConfig", MessageProvider.class);
 //        System.out.println(provider.getMessage());
+//
+//        GenericXmlApplicationContext context = new GenericXmlApplicationContext();
+//        context.load("classpath:app-context.xml");
+//        context.refresh();
+//        ConstructorConfusion constructorConfusion = (ConstructorConfusion) context.getBean("constructorConfusion");
+//        System.out.println(constructorConfusion);
+
+        com.spring.ch3.annotation.ConstructorConfusion constructorConfusion1 = (com.spring.ch3.annotation.ConstructorConfusion) ctx.getBean("constructorConfusionAnnotation");
+        System.out.println(constructorConfusion1);
     }
 }
--- src/main/java/com/spring/ch3/annotation/ConfigurableMessageProvider.java
+++ src/main/java/com/spring/ch3/annotation/ConfigurableMessageProvider.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service("message")
+@Service("messageProviderConfig")
 public class ConfigurableMessageProvider implements MessageProvider {
     private String message;
 
