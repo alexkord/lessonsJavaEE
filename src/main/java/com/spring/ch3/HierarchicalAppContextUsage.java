710013fd69d94e3ff4f1c3a70b3be394bc1d4fbf
alexkord <oleg.a.cherednichenko@gmail.com>
2018-05-21 23:26:28 +0300

    commit

--- src/main/java/com/spring/ch3/HierarchicalAppContextUsage.java
+++ src/main/java/com/spring/ch3/HierarchicalAppContextUsage.java
@@ -1,7 +1,24 @@
 package com.spring.ch3;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class HierarchicalAppContextUsage {
     public static void main(String[] args) {
+        GenericXmlApplicationContext parent = new GenericXmlApplicationContext();
+        parent.load("classpath:parent.xml");
+        parent.refresh();
 
+        GenericXmlApplicationContext child = new GenericXmlApplicationContext();
+        child.load("classpath:app-context.xml");
+        child.setParent(parent);
+        child.refresh();
+
+        SimpleTarget simpleTarget1 = (SimpleTarget) child.getBean("target1");
+        SimpleTarget simpleTarget2 = (SimpleTarget) child.getBean("target2");
+        SimpleTarget simpleTarget3 = (SimpleTarget) child.getBean("target3");
+
+        System.out.println(simpleTarget1.getVal());
+        System.out.println(simpleTarget2.getVal());
+        System.out.println(simpleTarget3.getVal());
     }
 }
