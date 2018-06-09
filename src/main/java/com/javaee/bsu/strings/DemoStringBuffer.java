b19bd178ef1de43996cc04fa79e9753e0aecc761
oleg_cherednichenko <oleg.a.cherednichenko@gmail.com>
2018-06-09 17:39:41 +0300

    commit

---
+++ src/main/java/com/javaee/bsu/strings/DemoStringBuffer.java
@@ -0,0 +1,21 @@
+package com.javaee.bsu.strings;
+
+public class DemoStringBuffer {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        System.out.println("length -> " + sb.length());
+        System.out.println("capacity -> " + sb.capacity());
+
+        sb.append("Java");
+        System.out.println("string " + sb);
+        System.out.println("length -> " + sb.length());
+        System.out.println("capacity -> " + sb.capacity());
+
+        sb.append("Internationalization");
+        System.out.println("string " + sb);
+        System.out.println("length -> " + sb.length());
+        System.out.println("capacity -> " + sb.capacity());
+
+        System.out.println("reverse-> " + sb.reverse());
+    }
+}
