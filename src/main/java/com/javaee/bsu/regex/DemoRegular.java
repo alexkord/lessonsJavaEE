26fa7cb806d120d455075cb001094ef0ada6ff8c
oleg_cherednichenko <oleg.a.cherednichenko@gmail.com>
2018-06-09 18:03:06 +0300

    commit

---
+++ src/main/java/com/javaee/bsu/regex/DemoRegular.java
@@ -0,0 +1,13 @@
+package com.javaee.bsu.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DemoRegular {
+    public static void main(String[] args) {
+        Pattern p1 = Pattern.compile("a+y");
+        Matcher m1 = p1.matcher("aaay");
+        boolean b = m1.matches();
+        System.out.println(b);
+    }
+}
